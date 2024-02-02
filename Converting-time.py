d3e924faed47eded87aa4f5b1407d34d47c77257
Hadjab95 <154864417+Hadjab95@users.noreply.github.com>
2024-02-01 22:45:54 -0700

    More codes

---
+++ Converting-time.py
@@ -0,0 +1,11 @@
+#This will translate a time expressed in days, hours, minutes and seconds into time expressed in seconds
+
+days = float(input("Enter the # of days: "))
+hours =  float(input("Enter the # of hours: ")) 
+minutes = float(input("Enter the # of minutes: "))
+seconds = float(input("Enter the # of seconds: "))
+
+print(f"You entered {days} days, {hours} hours, {minutes} minutes and {seconds} seconds")
+total_in_seconds = days*86400 + hours*3600 + minutes*60 + seconds
+
+print(f"That's {total_in_seconds} seconds")
