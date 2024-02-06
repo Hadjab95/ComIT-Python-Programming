b6f34b234f75aac19be923c2a1d9f0d5fdee1188
Hadjab95 <154864417+Hadjab95@users.noreply.github.com>
2024-02-05 18:46:11 -0700

    Algorithm to translate time in seconds

--- time_to_secs.py
+++ time_to_secs.py
@@ -9,3 +9,4 @@ print(f"You entered {days} days, {hours} hours, {minutes} minutes and {seconds} 
 total_in_seconds = days*86400 + hours*3600 + minutes*60 + seconds
 
 print(f"That's {total_in_seconds} seconds")
+
