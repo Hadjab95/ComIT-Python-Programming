9843d26a273f2f0b58a9fc4dcd9ca4eca14452ba
Hadjab95 <154864417+Hadjab95@users.noreply.github.com>
2024-02-06 12:22:46 -0700

    This algorithm will translate seconds into days, hours, minutes and seconds

--- time_to_days.py
+++ time_to_days.py
@@ -1,7 +1,26 @@
 #This Algorithm will translate a time expressed in seconds to a time expressed in days, hours, minutes and seconds.
+
+TotalSeconds = input("Enter the number of seconds: ")
+
+Timeafterdays = TotalSeconds % 86400
+Days = (TotalSeconds - Timeafterdays)/86400
+
+Timeafterhours = Timeafterdays % 3600
+Hours = (Timeafterdays - Timeafterhours)/3600
+
+Remainingseconds = Timeafterhours % 60
+Minutes = (Timeafterhours - Remainingseconds)/60
+
+print(f" {TotalSeconds} = {Days} days, {Hours} hours, {Minutes} minutes  ")
+
+
+
+
+
+
 seconds = int(input("Enter the number of seconds: "))
 mins= seconds/60
 hours = seconds/3600
 days = seconds/86400
 
-print(f"{seconds} seconds equals: {days} days, {hours} hours, {mins} minutes and {seconds}.")
+#print(f"{seconds} seconds = {days} days, {hours} hours, {mins} minutes and {seconds} seconds.")
