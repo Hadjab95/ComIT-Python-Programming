2a22ea5a823712dc993440930e2047d5301a1a36
Hadjab95 <154864417+Hadjab95@users.noreply.github.com>
2024-02-05 18:38:12 -0700

    This code determines if 2 numbers are divisible

--- Divisible-nums.py
+++ Divisible-nums.py
@@ -1,22 +1,9 @@
 #This algorithm will determine if an "N" number is divibles by another "M"
 
-#N = int(input("Enter a number: "))
-#M = int(input("Enter another number: "))
-#result = N / M
-#if result%2 == 0:
-    #print("These numbers are divisible") 
-#else:
-    #print("These numbers are not divisible")
-
-#Same Algorithm using a function
-
-def divisible_numbers(n, m):
-    return n%m == 0
-    
-n = 25
-m = 5
-result = divisible_numbers(n, m)
-print(f" {n} is divisible by {m}: {result}")
-
-
-    
+N = int(input("Enter a number: "))
+M = int(input("Enter another number: "))
+result = N / M
+if result%2 == 0:
+    print("These numbers are divisible") 
+else:
+    print("These numbers are not divisible")
